f8008a8ea0c29883a1b21dd0da2670d83eadc2b3
Alpesh Jain <162681308+alpzj@users.noreply.github.com>
2026-03-29 23:50:31 +0530

    Create pySpark.py

---
+++ Phase1/pySpark.py
@@ -0,0 +1,13 @@
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName('Spark Playground').getOrCreate()
+customers=spark.createDataFrame([
+     (1, "Ravi", "Hyderabad", 25),
+    (2, "Sita", "Chennai", 32),
+    (3, "Arun", "Hyderabad", 28)
+  ],["customer_id", "customer_name", "city", "age"])
+# queries
+customers.show()
+customers.filter(customers.city=="Chennai").show()
+customers.filter(customers.age>25).show()
+customers.select("customer_name", "city").show()
+customers.groupBy("city").count().show()
